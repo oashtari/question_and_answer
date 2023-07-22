a91005bf52aae9ad645e4b195c7755b37f99f8de
omidashtari <oashtari@gmail.com>
2023-07-22 06:11:22 -0700

    updated the update and delete functions and routes

--- src/routes/question.rs
+++ src/routes/question.rs
@@ -95,19 +95,33 @@ pub async fn update_question(
     store: Store,
     question: Question,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    match store.questions.write().await.get_mut(&QuestionId(id)) {
-        Some(q) => *q = question,
-        None => return Err(warp::reject::custom(Error::QuestionNotFound)),
-    }
-    Ok(warp::reply::with_status("Question udpated", StatusCode::OK))
+    let res = match store.update_question(question, id).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(Error::DatabaseQueryError(e))),
+    };
+    Ok(warp::reply::json(&res))
+
+    // match store.questions.write().await.get_mut(&QuestionId(id)) {
+    //     Some(q) => *q = question,
+    //     None => return Err(warp::reject::custom(Error::QuestionNotFound)),
+    // }
+    // Ok(warp::reply::with_status("Question udpated", StatusCode::OK))
 }
 
 pub async fn delete_question(id: i32, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
-    match store.questions.write().await.remove(&QuestionId(id)) {
-        Some(_) => Ok(warp::reply::with_status(
-            "Question deleted.",
-            StatusCode::OK,
-        )),
-        None => Err(warp::reject::custom(Error::QuestionNotFound)),
+    if let Err(e) = store.delete_question(id).await {
+        return Err(warp::reject::custom(Error::DatabaseQueryError(e)));
     }
+    Ok(warp::reply::with_status(
+        format!("Question {} deleted", id),
+        StatusCode::OK,
+    ))
+
+    // match store.questions.write().await.remove(&QuestionId(id)) {
+    //     Some(_) => Ok(warp::reply::with_status(
+    //         "Question deleted.",
+    //         StatusCode::OK,
+    //     )),
+    //     None => Err(warp::reject::custom(Error::QuestionNotFound)),
+    // }
 }
--- src/store.rs
+++ src/store.rs
@@ -80,6 +80,40 @@ impl Store {
             Err(e) => Err(e),
         }
     }
+
+    pub async fn update_question(
+        &self,
+        question: Question,
+        question_id: i32,
+    ) -> Result<Question, sqlx::Error> {
+        match sqlx::query("UPDATE questions SET title = $1, content = $2, tags = $3 WHERE id = $4 RETURNING id, table, content, tags")
+        .bind(question.title)
+        .bind(question.content)
+        .bind(question.tags)
+        .bind(question_id)
+        .map(|row: PgRow| Question {
+            id: QuestionId(row.get("id")),
+            title: row.get("title"),
+            content: row.get("content"),
+            tags: row.get("tags"),
+        })
+        .fetch_one(&self.connection)
+        .await {
+            Ok(question) => Ok(question),
+            Err(e) => Err(e)
+        }
+    }
+
+    pub async fn delete_question(&self, question_id: i32) -> Result<bool, sqlx::Error> {
+        match sqlx::query("DELETE from questions WHERE id = $1")
+            .bind(question_id)
+            .execute(&self.connection)
+            .await
+        {
+            Ok(_) => Ok(true),
+            Err(e) => Err(e),
+        }
+    }
     // fn add_question(mut self, question: Question) -> Self {
     //     self.questions.insert(question.id.clone(), question);
     //     self
