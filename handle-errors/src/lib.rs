6579177bc56fdbc43b90b63d74da19d04d721c26
omidashtari <oashtari@gmail.com>
2023-08-22 06:18:42 -0700

    added additional error handling for external API issues

--- handle-errors/src/lib.rs
+++ handle-errors/src/lib.rs
@@ -1,4 +1,5 @@
 use reqwest::Error as ReqwestError;
+use reqwest_middleware::Error as MiddlewareReqwestError;
 use warp::{
     filters::{body::BodyDeserializeError, cors::CorsForbidden},
     http::StatusCode,
@@ -15,7 +16,9 @@ pub enum Error {
     MissingParameters,
     // QuestionNotFound,
     DatabaseQueryError,
-    ExternalAPIError(ReqwestError),
+    ReqwestAPIError(ReqwestError),
+    MiddlewareReqwestAPIError(MiddlewareReqwestError),
+    // ExternalAPIError(ReqwestError),
     ClientError(APILayerError),
     ServerError(APILayerError),
 }
@@ -41,7 +44,8 @@ impl std::fmt::Display for Error {
             Error::MissingParameters => write!(f, "Missing parameter."),
             // Error::QuestionNotFound => write!(f, "Question not found."),
             Error::DatabaseQueryError => write!(f, "Cannot update, invalid data."),
-            Error::ExternalAPIError(err) => write!(f, "Cannot execute: {}", err),
+            Error::ReqwestAPIError(err) => write!(f, "External API error: {}", err),
+            Error::MiddlewareReqwestAPIError(err) => write!(f, "External API error: {}", err),
             Error::ClientError(err) => write!(f, "External client error: {}", err),
             Error::ServerError(err) => write!(f, "External server error: {}", err),
         }
@@ -60,10 +64,16 @@ pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
             crate::Error::DatabaseQueryError.to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
-    } else if let Some(crate::Error::ExternalAPIError(e)) = r.find() {
+    } else if let Some(crate::Error::ReqwestAPIError(e)) = r.find() {
         event!(Level::ERROR, "{}", e);
         Ok(warp::reply::with_status(
             "Internal server error ONE".to_string(),
+            StatusCode::INTERNAL_SERVER_ERROR,
+        ))
+    } else if let Some(crate::Error::MiddlewareReqwestAPIError(e)) = r.find() {
+        event!(Level::ERROR, "{}", e);
+        Ok(warp::reply::with_status(
+            "Internal server error.".to_string(),
             StatusCode::INTERNAL_SERVER_ERROR,
         ))
     } else if let Some(crate::Error::ClientError(e)) = r.find() {
